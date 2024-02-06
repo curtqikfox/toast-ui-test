d77d0f188e871abc9d8a06bc39aefbcefee4eef1
cpk42 <cpkrommenhoek@yahoo.com>
2024-02-06 09:40:31 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/react-editor';
+
+function textContentChanged(content: string) {
+  console.log(content);
+
+}
+
+class App extends React.Component {
+  editorRef = React.createRef() as any;
+
+  handleClickButton = () => {
+    console.log(this.editorRef.current.getInstance());
+    // console.log(this.editorRef.current.getInstance().GetMarkdown());
+    console.log(this.editorRef.current.getRootElement());
+    console.log(this.editorRef.current.getRootElement());
+
+    this.editorRef.current.getRootElement().classList.add('my-editor-root');
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Editor
+          onChange={textContentChanged}
+          usageStatistics={false}
+          ref={this.editorRef}
+
+        />
+        <button onClick={this.handleClickButton}>Click!</button>
+
+      </div>
+    );
+  }
 }
 
 export default App;
